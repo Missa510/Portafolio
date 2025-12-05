3e6e3ad2f17d1f96e0330d4d9309a8c6f7db75aa
Missa510 <sjqv05@gmail.com>
2025-12-05 18:25:10 +0000

    Adapter problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +15,5 @@ export default defineConfig({
       locales: ["es", "en"],
   },
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
